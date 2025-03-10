9acfb931e43a75ddcffd8871ab282e309a8d4a83
Ajay <ajaykommana123@gmail.com>
2025-03-10 21:40:58 +0530

    API output modified

--- classifier_api.py
+++ classifier_api.py
@@ -9,7 +9,7 @@ from flask_cors import CORS
 import time
 
 app = Flask(__name__)
-CORS(app, origins=["http://localhost:9000"])
+CORS(app, origins=["http://localhost:9000","https://25pxhl0v-9000.inc1.devtunnels.ms"])
 
 # Load the trained model
 model_dict = pickle.load(open('D:\B Tech\Major Project\Sign-Language-Recognition-Model\model.p', 'rb'))
@@ -18,8 +18,10 @@ model = model_dict['model']
 mp_hands = mp.solutions.hands
 hands = mp_hands.Hands(static_image_mode=True, min_detection_confidence=0.7)
 
-local_chars = []
-remote_chars = []
+# local_chars = []
+# remote_chars = []
+local_chars = ""
+remote_chars = ""
 
 local = {"left": 475, "top": 345, "width": 660, "height": 550}
 remote = {"left": 1135, "top": 345, "width": 660, "height": 550}
@@ -31,23 +33,24 @@ def get_predicted_chars():
     return jsonify({'local_chars': local_chars, 'remote_chars': remote_chars})
 
 
-def remove_items_periodically():
-    """To remove the out-timed captions"""
-    global local_chars, remote_chars
-    while True:
-        time.sleep(2)  # Wait for 2 seconds
-        if local_chars:
-            local_chars.pop(0)
-        if remote_chars:
-            remote_chars.pop(0)
+# def remove_items_periodically():
+#     """To remove the out-timed captions"""
+#     global local_chars, remote_chars
+#     while True:
+#         time.sleep(2)  # Wait for 2 seconds
+#         if local_chars:
+#             local_chars.pop(0)
+#         if remote_chars:
+#             remote_chars.pop(0)
 
 
 def process_video():
     """Captures the live video and predict the sign"""
     global local, remote
-    turn = True
+    global local_chars, remote_chars
     with mss.mss() as sct:
         while True:
+            time.sleep(0.5)
             data_aux = []
             x_ = []
             y_ = []
@@ -82,15 +85,17 @@ def process_video():
                 prediction = model.predict([np.asarray(data_aux)])
                 predicted_character = prediction[0]
                 if remote['left'] == 475:
-                    if local_chars.count(predicted_character) < 1:
-                        local_chars.append(predicted_character)
+                    # if local_chars.count(predicted_character) < 1:
+                    #     local_chars.append(predicted_character)
+                    local_chars = predicted_character
                 else:
-                    if remote_chars.count(predicted_character) < 1:
-                        remote_chars.append(predicted_character)
-                if len(local_chars) > 5:
-                    local_chars.pop(0)
-                if len(remote_chars) > 5:
-                    remote_chars.pop(0)
+                    # if remote_chars.count(predicted_character) < 1:
+                    #     remote_chars.append(predicted_character)
+                    remote_chars = predicted_character
+                # if len(local_chars) > 5:
+                #     local_chars.pop(0)
+                # if len(remote_chars) > 5:
+                #     remote_chars.pop(0)
 
             if cv2.waitKey(1) & 0xFF == ord('q'):
                 break
@@ -102,5 +107,5 @@ def process_video():
 # Main
 if __name__ == '__main__':
     threading.Thread(target=app.run, kwargs={'debug': True, 'use_reloader': False}).start()
-    threading.Thread(target=remove_items_periodically, daemon=True).start()
+    # threading.Thread(target=remove_items_periodically, daemon=True).start()
     process_video()
