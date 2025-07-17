e2e2731c3adab613a2835dfa809a71dfe8dcf673
Ajay <ajaykommana123@gmail.com>
2025-07-17 10:02:53 +0530

    README file added

--- classifier_api.py
+++ classifier_api.py
@@ -18,8 +18,6 @@ model = model_dict['model']
 mp_hands = mp.solutions.hands
 hands = mp_hands.Hands(static_image_mode=True, min_detection_confidence=0.7)
 
-# local_chars = []
-# remote_chars = []
 local_chars = ""
 remote_chars = ""
 
@@ -31,18 +29,6 @@ remote = {"left": 1135, "top": 345, "width": 660, "height": 550}
 def get_predicted_chars():
     """Returns predicted words as json format"""
     return jsonify({'local_chars': local_chars, 'remote_chars': remote_chars})
-
-
-# def remove_items_periodically():
-#     """To remove the out-timed captions"""
-#     global local_chars, remote_chars
-#     while True:
-#         time.sleep(2)  # Wait for 2 seconds
-#         if local_chars:
-#             local_chars.pop(0)
-#         if remote_chars:
-#             remote_chars.pop(0)
-
 
 def process_video():
     """Captures the live video and predict the sign"""
@@ -58,54 +44,34 @@ def process_video():
             screenshot = sct.grab(remote)
             frame = np.array(screenshot)
             frame = cv2.cvtColor(frame, cv2.COLOR_BGRA2BGR)
-
-            # H, W, _ = frame.shape
             frame_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
             results = hands.process(frame_rgb)
-
             if results.multi_hand_landmarks:
                 for hand_landmarks in results.multi_hand_landmarks:
                     for i in range(len(hand_landmarks.landmark)):
                         x = hand_landmarks.landmark[i].x
                         y = hand_landmarks.landmark[i].y
-
                         x_.append(x)
                         y_.append(y)
-
                     for i in range(len(hand_landmarks.landmark)):
                         data_aux.append(x_[i] - min(x_))
                         data_aux.append(y_[i] - min(y_))
-
                 expected_length = model.n_features_in_
                 if len(data_aux) < expected_length:
                     data_aux += [0] * (expected_length - len(data_aux))
                 elif len(data_aux) > expected_length:
                     data_aux = data_aux[:expected_length]
-
                 prediction = model.predict([np.asarray(data_aux)])
                 predicted_character = prediction[0]
                 if remote['left'] == 475:
-                    # if local_chars.count(predicted_character) < 1:
-                    #     local_chars.append(predicted_character)
                     local_chars = predicted_character
                 else:
-                    # if remote_chars.count(predicted_character) < 1:
-                    #     remote_chars.append(predicted_character)
                     remote_chars = predicted_character
-                # if len(local_chars) > 5:
-                #     local_chars.pop(0)
-                # if len(remote_chars) > 5:
-                #     remote_chars.pop(0)
-
             if cv2.waitKey(1) & 0xFF == ord('q'):
                 break
-
     hands.close()
     cv2.destroyAllWindows()
-
-
 # Main
 if __name__ == '__main__':
     threading.Thread(target=app.run, kwargs={'debug': True, 'use_reloader': False}).start()
-    # threading.Thread(target=remove_items_periodically, daemon=True).start()
     process_video()
